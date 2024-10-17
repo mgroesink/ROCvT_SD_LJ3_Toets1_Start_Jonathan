e74195a55e5fd49b83fa49ff6e8bd76e2465b7ab
0340671 <0340671@student.rocvantwente.nl>
2024-10-17 09:31:43 +0200

    Commit 2 van Jonathan

--- I1SD_Toets1_Start/Controllers/OpdrachtController.cs
+++ I1SD_Toets1_Start/Controllers/OpdrachtController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using I1SD_Toets1_Start;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Toets1_Start.Controllers
 {
@@ -7,14 +8,29 @@ namespace Toets1_Start.Controllers
         public IActionResult Opdracht1()
         {
             // Hier komt jouw uitwerking van opdracht 1
-            ViewBag.Naam = "Jouw naam";
-            return Content("Welkom bij deze ASP-NET / C# toets");
+            ViewBag.Naam = "Jonathan";
+            return View();
         }
+        [HttpPost]
+        public IActionResult Opdracht1(int Value1)
+        {
+            // Hier komt jouw uitwerking van opdracht 1
+            ViewBag.Naam = "Jonathan";
+
+            if (Value1 >= 10 && Value1 <= 100)
+            {
+                ViewBag.awnser = MyFunctions.CreateNumberSequence(Value1);
+            }
+
+
+            return View();
+        }
+
 
         public IActionResult Opdracht2()
         {
             // Hier komt jouw uitwerking van opdracht 2
-            ViewBag.Naam = "Jouw naam";
+            ViewBag.Naam = "Jonathan";
             return Content("Welkom bij deze ASP-NET / C# toets");
         }
     }
--- I1SD_Toets1_Start/MyFunctions.cs
+++ I1SD_Toets1_Start/MyFunctions.cs
@@ -10,5 +10,28 @@
         {
             return "Welkom bij deze ASP-NET / C# toets";
         }
+
+        public static List<int> CreateNumberSequence(int value1)
+        {
+            List<int> awnser = new List<int>();
+
+            for (awnser = value1;  < 0;)
+            {
+
+            }
+
+
+                if (value1 % 2 == 0)
+                {
+                    value1 = value1 / 2;
+                }
+                else
+                {
+                    value1 = value1 * 3 + 1;
+                }
+            
+
+            return awnser;
+        }
     }
 }
